d44af4b53d72df245acced0ae663762b1ca6ff82
omochimetaru <omochi.metaru@gmail.com>
2019-02-04 09:30:53 +0900

    less return

--- Sources/SwCombinator/main.swift
+++ Sources/SwCombinator/main.swift
@@ -1,21 +1,19 @@
 class Expression {
     static let E: P<Int> = rule { A }
     static let A: P<Int> = rule {
-        (M + ((s(literal:"+") + M) | (s(literal:"-") + M)).rep0()).map{v in
-            let (l, rs) = v
-            return rs.reduce(l, {(l, b) in
+        (M + ((s(literal:"+") + M) | (s(literal:"-") + M)).rep0()).map{ (l, rs) in
+            rs.reduce(l) { (l, b) in
                 let (op, r) = b
                 return op == "+" ? l + r : l - r
-            })
+            }
         }
     }
     static let M: P<Int> = rule {
-        (P + ((s(literal:"*") + P) | (s(literal:"/") + P)).rep0()).map{v in
-            let (l, rs) = v
-            return rs.reduce(l, {(l, b) in
+        (P + ((s(literal:"*") + P) | (s(literal:"/") + P)).rep0()).map{ (l, rs) in
+            rs.reduce(l) { (l, b) in
                 let (op, r) = b
                 return op == "*" ? l * r : l / r
-            })
+            }
         }
     }
     static let P: P<Int> = rule {
